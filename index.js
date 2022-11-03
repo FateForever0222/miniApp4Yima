94b206bd0f4ee1beb7239471d640ccd66340de43
fyh <2271470941@qq.com>
2022-11-03 17:28:05 +0800

    test

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const ip = require('ip')
 const app = express()
+const request =require('request')
 app.use(express.json())
 
 app.all('/', (req, res) => {
   res.send({
     ip: ip.address(),
     headers: req.headers,
+    test:req.data,
     env: process.env
+  })
+})
+
+app.all('/phone',(req,res)=>{
+  // let url=`https://api.weixin.qq.com/wxa/business/getuserphonenumber?code=${code}`
+  // let req_wx=http.request(url,function(res))
+  res.send({
+    headers: req.headers
   })
 })
 
