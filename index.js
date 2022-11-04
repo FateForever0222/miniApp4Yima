732037a86d1e7bcd00f632e52e6864f49192205d
fyh <2271470941@qq.com>
2022-11-04 11:25:07 +0800

    test

--- index.js
+++ index.js
@@ -13,26 +13,29 @@ app.all('/', (req, res) => {
   })
 })
 
-app.all('/phone',(req,res)=>{
-  var code=req.headers.code
-  let url=`https://api.weixin.qq.com/wxa/business/getuserphonenumber?code=${code}`
-  var body
-  let req_wx=new Promise((resolve, reject) => {
-    request({
-      method: 'POST',
-      // url: 'http://api.weixin.qq.com/wxa/msg_sec_check?access_token=TOKEN',
-      url: url, // 这里就是少了一个token
-    },function (error, response) {
-      console.log('接口返回内容', response.body)
-      body=resolve(JSON.parse(response.body))
-    })
-  })
-  res.send({
-    ip: ip.address(),
-    headers: req.headers,
-    body:body
-  })
-})
+app.post('/phone', (req, res) => {
+  // 拼接 Header 中的 x-wx-openid 到接口中
+  const api = `http://api.weixin.qq.com/wxa/getopendata?openid=${req.headers['x-wx-from-openid']}`;
+  request(api, {
+    method: 'POST',
+    body: JSON.stringify({
+      cloudid_list: [req.body.cloudid], // 传入需要换取的 CloudID
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }, (err, resp, body) => {
+    try {
+      const data = JSON.parse(body).data_list[0]; // 从回包中获取手机号信息
+      const phone = JSON.parse(data.json).data.phoneNumber;
+      // 将手机号发送回客户端，此处仅供示例
+      // 实际场景中应对手机号进行打码处理，或仅在后端保存使用
+      res.send(phone);
+    } catch (error) {
+      res.send('get phone failed');
+    }
+  });
+});
 
 const port = process.env.PORT || 80
 
