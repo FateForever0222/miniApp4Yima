bc6db595864f07cd04a462e546c17640c768719b
fyh <2271470941@qq.com>
2022-11-04 19:56:55 +0800

    getphone

--- index.js
+++ index.js
@@ -15,19 +15,16 @@ app.all('/', (req, res) => {
 
 app.post('/phone', (req, res) => {
   // 拼接 Header 中的 x-wx-openid 到接口中
-  const api = `http://api.weixin.qq.com/wxa/getopendata?openid=${req.headers['x-wx-from-openid']}`;
+  const api = `http://api.weixin.qq.com/wxa/business/getuserphonenumber?code=${req.headers['code']}`;
   request(api, {
     method: 'POST',
-    body: JSON.stringify({
-      cloudid_list: [req.body.cloudid], // 传入需要换取的 CloudID
-    }),
     headers: {
       'Content-Type': 'application/json',
     },
   }, (err, resp, body) => {
     try {
-      const data = JSON.parse(body).data_list[0]; // 从回包中获取手机号信息
-      const phone = JSON.parse(data.json).data.phoneNumber;
+      const data = JSON.parse(body).phone_info; // 从回包中获取手机号信息
+      const phone = JSON.parse(data.json).purePhoneNumber;
       // 将手机号发送回客户端，此处仅供示例
       // 实际场景中应对手机号进行打码处理，或仅在后端保存使用
       res.send(phone);
